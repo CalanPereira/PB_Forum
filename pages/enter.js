6fc302dd5564d0b8d9125c3f55dbc56d57f943a8
repegfrost <61025991+repegfrost@users.noreply.github.com>
2023-02-01 17:33:12 +0530

    debounce and form validation updated

--- pages/enter.js
+++ pages/enter.js
@@ -1,8 +1,12 @@
 //import { auth,signInWithPopup , googleAuthProvider } from '../lib/firebase'
 import { getAuth ,signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "@/lib/context";
-import { auth } from "@/lib/firebase";
+import { auth, firestore } from "@/lib/firebase";
+import { useCallback } from "react";
+import { async } from "@firebase/util";
+import debounce from 'lodash.debounce';
+import { doc,getDoc, writeBatch } from "firebase/firestore";
 
 export default function EnterPage({ }){
     const { user, username } = useContext(UserContext);
@@ -37,5 +41,97 @@ function SignOutButton(){
 }
 
 function UsernameForm(){
+    const [formValue, setFormValue] = useState('');
+    const [isValid, setIsValid] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const { user, username } = useContext(UserContext);
+
+    useEffect (() => {
+        checkUsernamse(formValue);
+    },[formValue]);
+
+    const onChange = (e) =>{
+        const val = e.target.value.toLowerCase();
+        const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+        if(val.length < 3)
+        {
+            setFormValue(val);
+            setLoading(false);
+            setIsValid(false);
+        }
+
+        if(re.test(val))
+        {
+            setFormValue(val);
+            setLoading(true);
+            setIsValid(false);
+        }
+    };
     
+    const checkUsernamse = useCallback(
+        debounce(async (username) => {
+        if(username.length >= 3){
+            const ref= doc(firestore,'usernames/${username}');
+            const { exists } = await getDoc(ref);
+            console.log('Firestore read executed');
+            setIsValid(!exists);
+            setLoading(false);
+        }
+    },500),
+    []
+    );
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        const userDoc = doc(firestore,`users/${user.uid}`);
+        const usernameDoc = doc(firestore,`username/${formValue}`);
+
+        const batch = writeBatch(firestore);
+        batch.set(userDoc, { username: formValue, photoURL: user.photoURL, displayName: user,displayName});
+        batch.set(usernameDoc, { uid: user.uid});
+
+        await batch.commit();
+    };
+
+    return (
+        !username && (
+            <section>
+                <h3>Choose Username</h3>
+                <form onSubmit={onSubmit}>
+
+                    <input name="username" placeholder="username" value={formValue} onChange={onChange} />
+                    
+                    <UsernameMessage username={formValue} isValid={isValid} loading={loading} />
+
+                    <button type="submit" className="btn-green" disabled={!isValid}>
+                        Choose
+                    </button>
+
+                    <h3>Debug State</h3>
+                    <div>
+                        Username : {formValue}
+                        <br />
+                        Loading : {loading.toString()}
+                        <br />
+                        Username Valid : {isValid.toString()}
+                    </div>
+                </form>
+            </section>
+        )
+    );
+}
+
+function UsernameMessage({ username, isValid, loading}){
+    if(loading){
+        return <p>Checking...</p>;
+    }else if(isValid){
+        return <p className="text-sucess">{username} is available</p>;
+    }else if(username && !isValid){
+        return <p className="text-danger">That username is taken!</p>;
+    }else{
+        return <p></p>;
+    }
 }
